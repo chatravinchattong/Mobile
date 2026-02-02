1c003cc566c4bde0bf436de19497fda1c2c08e4a
chatravinchattong <101159845+chatravinchattong@users.noreply.github.com>
2026-02-02 10:06:50 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:/flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,145 +11,110 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const CalculatorPage(),
+      title: 'Flutter Demo'
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'List Example')
     );
   }
 }
 
-class CalculatorPage extends StatefulWidget {
-  const CalculatorPage({super.key});
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
 
   @override
-  State<CalculatorPage> createState() => _CalculatorPageState();
+  Stare<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _CalculatorPageState extends State<CalculatorPage> {
-  String display = "0";
-  double firstNum = 0;
-  String operator = "";
+class data {
+  late int id;
+  late String name;
+  late DataTime t;
+  data(this.id, this.name, this.t);
+}
 
-  // ปุ่มทั้งหมดเรียงแบบเดียวกับรูป
-  final List<String> buttons = [
-    "7", "8", "9", "+",
-    "4", "5", "6", "-",
-    "1", "2", "3", "*",
-    "0", "C", "=", "/",
-  ];
-
-  void onPressed(String value) {
-    setState(() {
-      if (value == "C") {
-        display = "0";
-        firstNum = 0;
-        operator = "";
-        return;
-      }
-
-      if (value == "+" || value == "-" || value == "*" || value == "/") {
-        firstNum = double.tryParse(display) ?? 0;
-        operator = value;
-        display = "0";
-        return;
-      }
-
-      if (value == "=") {
-        double secondNum = double.tryParse(display) ?? 0;
-        double result = 0;
-
-        switch (operator) {
-          case "+": result = firstNum + secondNum; break;
-          case "-": result = firstNum - secondNum; break;
-          case "*": result = firstNum * secondNum; break;
-          case "/":
-            result = secondNum != 0 ? firstNum / secondNum : double.nan;
-            break;
-        }
-
-        display = (result.isNaN) ? "Error" : result.toString();
-        operator = "";
-        return;
-      }
-
-      // ถ้าเป็นตัวเลข
-      if (display == "0") {
-        display = value;
-      } else {
-        display += value;
-      }
-    });
-  }
+class _MyHomePageState extends State<MyHomePage> {
+  String txt = 'N/A';
+  List<data> mylist = <data>[];
+  int img = 0;
+  var list = ['one', 'two', 'three', 'four'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF7EFFF),
-      body: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            const Text(
-              "Calculator",
-              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              display,
-              style: const TextStyle(
-                fontSize: 60,
-                color: Colors.red,
-                fontWeight: FontWeight.bold,
+      appBar: AppBar(
+        title: Text(winget.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Row(
+            children: [
+              Radio(
+                value: 1,
+                groupValue: img,
+                onChanged: (int? value) {
+                  setState(() {
+                    img = 1;
+                  });
+                }),
+              const CircleAvatar(
+                radius: 20,
+                backgroundImage: AssetImage('asset/imagesif.png'),
               ),
-            ),
-            const SizedBox(height: 20),
-
-            Expanded(
-              child: GridView.builder(
-                padding: const EdgeInsets.all(20),
-                itemCount: buttons.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  crossAxisSpacing: 15,
-                  mainAxisSpacing: 15,
-                ),
-                itemBuilder: (context, index) {
-                  String btn = buttons[index];
-
-                  Color bgColor = const Color(0xFFDCCBFF); // สีม่วงอ่อน
-                  Color textColor = Colors.black;
-
-                  if (btn == "C") {
-                    bgColor = const Color(0xFF8F63FF); // ปุ่มสีม่วงเข้ม
-                    textColor = Colors.white;
-                  }
-
-                  if (btn == "=") {
-                    bgColor = Colors.white;
-                  }
-
-                  return ElevatedButton(
-                    onPressed: () => onPressed(btn),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: bgColor,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25),
+            ],
+          ),
+          const Textfield(),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                txt = 'Add item Success';
+                mylist.add(data(img, '1', DataTime.now()));
+              });
+            },
+            child: const Text('Add Item')),
+          Text(
+            txt,
+            textScaleFactor: 2,
+          ),
+          SizeBox(
+            width: double.infinity,
+            height: 550,
+            child: ListView.builder(
+              itemCount: mylist.length,
+              itemBuilder: (context,index) {
+                return SizeBox(
+                  width: double.infinity,
+                  height: 80,
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                    color:
+                        Colors.primaries[index % Coloes.primaries.length],
+                    child: ListTitle(
+                      leading: const CircleAvatar(
+                        radius: 30,
+                        backgroundImage:
+                          AssetImage('assets/imges/rocket.png'),
                       ),
-                      shadowColor: Colors.transparent,
-                    ),
-                    child: Text(
-                      btn,
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                        color: textColor,
+                      title: Text('Title Text (${mylist[index].id})'),
+                      subtitle: Text(mylist[index].t.toString()),
+                      trailing: const Icon(Icons.delete_rounded),
+                      onTap: () {
+                        setState(() {
+                          txt = "Title Text ($index) is remove";
+                          mylist.removeAt(index);
+                          });
+                        },
                       ),
                     ),
                   );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+                }),
+            )
+          ]),
+        ));
   }
 }
+
